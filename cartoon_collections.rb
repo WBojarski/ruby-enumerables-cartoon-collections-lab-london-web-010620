bd84af1467764580646fa89c3ca7d6b3753e4475
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-28 20:42:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array_of_dwarves)
+
+	array_of_dwarves.each_with_index do |dwarf,index|
+		puts "#{index + 1}.#{dwarf}"
+	end
+
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+
+	summon = planeteer_calls.map do |n| 
+		"#{n.capitalize}!"
+	end 
+	summon
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_of_calls) 
+
+	check_characters = array_of_calls.inject {|call| if call.length < 4 true else }
+
+		
+	end
+	check_characters
 end
 
 def find_the_cheese# code an argument here
