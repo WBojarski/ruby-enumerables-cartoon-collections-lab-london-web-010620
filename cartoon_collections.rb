93e60e75506c3c532a064de0dac1807a8dc1f4cc
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-28 21:06:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,8 @@ end
 
 def long_planeteer_calls(array_of_calls) 
 
-	character_check = array_of_calls.any? do |call|
-		call.length < 4
-	end
-	character_check
+	array_of_calls.any? {|call| call.length > 4}
+	
 end
 
 def find_the_cheese# code an argument here
